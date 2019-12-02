8336e19b94d9f26a51d988583d6b710a9f9292d1
Madhav Aggarwal <madhav.taggarwal@gmail.com>
2019-12-01 23:20:30 -0600

    Just some little tweaks (try catch blocks)

--- app/src/main/java/com/example/studymate/ui/GeneralFunctions.java
+++ app/src/main/java/com/example/studymate/ui/GeneralFunctions.java
@@ -8,6 +8,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 public class GeneralFunctions {
     public static String getEmail(Activity context) {
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(context);
-        return acct.getEmail();
+        try {
+            return acct.getEmail();
+        } catch (Exception e) {
+            System.out.println("Could not return email of user");
+            e.printStackTrace();
+            return "";
+        }
     }
 }
