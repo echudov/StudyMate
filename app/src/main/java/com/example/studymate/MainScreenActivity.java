ccaad4622da04daacb01ee2cccb9e8f49eb86571
Madhav Aggarwal <madhav.taggarwal@gmail.com>
2019-12-12 15:54:37 -0600

    Cleaned up imports too

--- app/src/main/java/com/example/studymate/MainScreenActivity.java
+++ app/src/main/java/com/example/studymate/MainScreenActivity.java
@@ -4,11 +4,8 @@ import android.content.BroadcastReceiver;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Looper;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -21,14 +18,8 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MainScreenActivity extends AppCompatActivity {
 
